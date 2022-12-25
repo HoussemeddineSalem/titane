cef4f59b5bc76bba070cf53d4ad31cb0210246f5
Houssem SALEM <houssem.salem@wecraft.tn>
2022-12-25 21:31:36 +0100

    rename api

--- src/main/java/dev/titane/users/ApiMembersApplication.java
+++ src/main/java/dev/titane/users/ApiMembersApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiUsersApplication {
+public class ApiMembersApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiUsersApplication.class, args);
+		SpringApplication.run(ApiMembersApplication.class, args);
 	}
 
 }
--- src/test/java/dev/titane/users/ApiMembersApplicationTests.java
+++ src/test/java/dev/titane/users/ApiMembersApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApiUsersApplicationTests {
+class ApiMembersApplicationTests {
 
 	@Test
 	void contextLoads() {
